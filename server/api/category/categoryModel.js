d38086dc27c4cb50031bebade32408d8f781e846
Kevin Cheng <09chengk@gmail.com>
2016-03-29 18:28:47 -0700

    creates schemas for each model

--- server/api/category/categoryModel.js
+++ server/api/category/categoryModel.js
@@ -2,7 +2,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var CategorySchema = new Schema({
-
+  name: {
+    type: String,
+    required: true
+  }
 });
 
 module.exports = mongoose.model('category', CategorySchema);
